c5b856426888c2bb85c00d544cf5be959794c8d4
Canadian_BeeBee <108027171+CanadaBeeBee@users.noreply.github.com>
2022-08-14 17:45:53 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const txtAnim = document.querySelector('h1');
+
+new Typewritter ( txtAnim, {
+
+   // deletespeed : 20
+})
+
+
+    .typeString('Moi c\'est Nathan')
+    .pausefor(300)
+    .typeString (Héhé)
+    .start()
